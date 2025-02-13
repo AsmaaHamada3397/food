45091335343e3f38c421e2cb23ba96d5b8a4de02
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2025-02-13 06:20:40 +0200

    menu component is created

--- src/components/home/HealthyFood.vue
+++ src/components/home/HealthyFood.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <h1>Healthy food</h1>
+  <div class="grid sm:grid-cols-1 md:grid-cols-2 m-auto p-30 relative">
+    <div>
+      <img :src="container.images" alt="" class="w-[599px] h-[566]" />
+      <div
+        class="bg-[#474747] text-white h-[321px] w-[411px] p-10 rounded-2xl absolute left-80 bottom-12"
+      >
+        <div>
+          <h1 class="text-[24px] pb-9 font-medium">Come and visit us</h1>
+          <img :src="container.phone" alt="" class="inline w-[24px] h-[24px]" />
+          <span class="ps-4 text-[#F9F9F7]">(414) 857 - 0107</span>
+          <div class="pt-8">
+            <img :src="container.email" alt="" class="inline w-[24px] h-[24px]" />
+            <span class="ps-4 text-[#F9F9F7]">happytummy@restaurant.com</span>
+          </div>
+          <div class="pt-8 max-w-[90%] flex items-start">
+            <img :src="container.location" alt="" class="inline w-[24px] h-[24px]" />
+            <span class="ps-4 text-[#F9F9F7] leading-tight"
+              >837 W. Marshall Lane Marshalltown, IA 50158, Los Angeles</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1>hi</h1>
+    </div>
   </div>
 </template>
+
+<script>
+import image from '../../assets/images/home/image.svg'
+import phone from '../../assets/images/header/phone.svg'
+import email from '../../assets/images/header/mail.svg'
+import location from '../../assets/images/home/location-marker.svg'
+
+export default {
+  name: 'healthyFood',
+  props: {},
+  data() {
+    return {
+      container: {
+        images: image,
+        phone: phone,
+        email: email,
+        location: location,
+      },
+    }
+  },
+}
+</script>
--- src/components/home/OurMenu.vue
+++ src/components/home/OurMenu.vue
@@ -1,21 +1,69 @@
-<template>
-  <div class="text-center">
-    <h1 class="text-[#2C2F24] text-[55px] my-15">Browse Our Menu</h1>
-    <div class="grid grid-cols-4 w-[80%] place-items-center mx-auto text-center">
 
-      <!--Breakfast-->
-      <div class="border border-[#DBDFD0] p-5 rounded-[12px] h-[375px] w-[306px] text-center">
-        <img
-          class="w-24 h-24 rounded-full bg-[#DBDFD0] p-5
-           "
-          src="../../assets/images/home/menu/tea 1.svg"
-          alt=""
-        />
-        <h1 class="text-[#2C2F24] font-semibold py-4 text-[24px]">Breakfast</h1>
+ <template>
+  <div class="text-center">
+    <h1 class="text-[#2C2F24] text-[42px] my-15">Browse Our Menu</h1>
+
+    <!-- Grid container -->
+    <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 px-10 mx-auto text-center place-items-center">
+
+      <!-- Loop through cards -->
+      <div v-for="card in cards" :key="card.title" class="border border-[#DBDFD0] p-5 rounded-[12px] h-[375px] w-[306px] text-center">
+
+        <div class="relative text-center bg-[#DBDFD0] inline-block rounded-full h-[100px] w-[100px] mt-5">
+          <img class="w-[48px] h-[50px] my-6 absolute left-7 bottom-0" :src="card.image"/>
+        </div>
+
+        <h1 class="text-[#2C2F24] font-bold py-4 text-[24px]">{{ card.title }}</h1>
+        <p class="px-2 text-[#414536] text-md">{{ card.description }}</p>
+        <button class="text-[#AD343E] text-md font-medium pt-7 cursor-pointer">{{ card.buttonText }}</button>
       </div>
-      <h1>2</h1>
-      <h1>3</h1>
-      <h1>4</h1>
+
     </div>
   </div>
 </template>
+
+
+<script>
+import teaImage from "../../assets/images/home/menu/tea 1.svg"
+import drinks from "../../assets/images/home/menu/drinks.svg"
+import dishes from "../../assets/images/home/menu/m-dishes.svg"
+import desserts from "../../assets/images/home/menu/group.svg"
+
+export default {
+  name: 'HomeMenu',
+  data() {
+    return {
+     cards:[
+       {
+         title: 'Breakfast',
+         description: 'In the new era of technology we look in the future with certainty and pride for our life.',
+         buttonText: 'Explore Menu',
+         image: teaImage
+       },
+       {
+        title: "Main Dishes",
+        description: 'In the new era of technology we look in the future with certainty and pride for our life.',
+        buttonText: 'Explore Menu',
+        image: dishes
+       },
+       {
+        title: "Drinks",
+        description: 'In the new era of technology we look in the future with certainty and pride for our life.',
+        buttonText: 'Explore Menu',
+        image: drinks
+       },
+       {
+        title: "Desserts",
+        description: 'In the new era of technology we look in the future with certainty and pride for our life.',
+        buttonText: 'Explore Menu',
+        image: desserts
+       }
+     ]
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
