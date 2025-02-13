8b33d575f7c20e813029f71c78e4eef49eb63743
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2025-02-13 22:18:51 +0200

    our Delivery component is created

--- src/components/home/OurDelivery.vue
+++ src/components/home/OurDelivery.vue
@@ -1,7 +1,33 @@
 <template>
- <div>
-  <h1>
-    delivery
-  </h1>
+ <div class="bg-[#F9F9F7] grid sm:grid-cols-1 md:grid-cols-3 p-20">
+  <div class="">
+    <img src="../../assets/images/home/delivery/mid-shot-chef-holding-plate-with-pasta-making-ok-sign 1.svg" alt="">
+  </div>
+  <div>
+    <img class="px-5 pt-7" src="../../assets/images/home/delivery/sour-curry-with-snakehead-fish-spicy-garden-hot-pot-thai-food 1.svg" alt="">
+    <img class="px-5 pt-3" src="../../assets/images/home/delivery/sadj-iron-pot-with-various-salads 1.svg" alt="">
+  </div>
+  <div class="place-content-center">
+    <h1 class="text-[55px] text-[#2C2F24] leading-15">Fastest Food Delivery in City</h1>
+    <p class="text-[16px] text-[#414536] pt-5">Our visual designer lets you quickly
+      and of drag a down your way to customapps for both keep desktop.
+    </p>
+
+    <!--features-->
+    <div class="my-7">
+      <div class="flex items-center gap-3 mb-3">
+        <img src="../../assets/images/home/delivery/time.svg" class="h-[18px] w-[18px] bg-[#AD343E] p-1 rounded-full" alt="">
+        <p class="text-[14px] text-[#414536] font-medium pb-0.5">Delivery within 30 minutes</p>
+      </div>
+      <div class="flex items-center gap-3 mb-3">
+        <img src="../../assets/images/home/delivery/receipt-tax.svg" class="h-[18px] w-[18px] bg-[#AD343E] p-1 rounded-full" alt="">
+        <p class="text-[14px] text-[#414536] font-medium pb-0.5">Best Offer & Prices</p>
+      </div>
+      <div class="flex items-center gap-3 mb-3">
+        <img src="../../assets/images/home/delivery/online-services.svg" class="h-[18px] w-[18px] bg-[#AD343E] p-1 rounded-full" alt="">
+        <p class="text-[14px] text-[#414536] font-medium pb-0.5">Online Services Available</p>
+      </div>
+    </div>
+  </div>
  </div>
 </template>
--- src/components/home/OurEvents.vue
+++ src/components/home/OurEvents.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    <h1>We also offer unique services for your events</h1>
+  <div class="p-20">
+    <h1 class="text-[55px] font-semibold w-[50%] leading-15">We also offer unique services for your events</h1>
+
+    <div class="grid sm:grid-cols-1 lg:grid-cols-4 py-15 gap-4">
+      <div v-for="card in cards" :key="card.title">
+        <img :src="card.image"
+        alt="caterings"
+        class-="rounded-xl">
+        <h1 class="text-[#2C2F24] text-[24px] font-bold pt-4 pb-3">{{ card.title }}</h1>
+        <p class="text-[#414536] text-[16px]">{{ card.description }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+
+import caterings from "../../assets/images/home/services/kebab-set-table 1.svg"
+import birthdays from "../../assets/images/home/services/birthdays.svg"
+import weddings from "../../assets/images/home/services/happy-man-wife-sunny-day 1.svg"
+import events from "../../assets/images/home/services/events.svg"
+
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          title: "Caterings",
+          image: caterings,
+          description: "In the new era of technology we look in the future with certainty for life."
+        },
+        {
+          title: "Birthdays",
+          image: birthdays,
+          description: "In the new era of technology we look in the future with certainty for life."
+        },
+        {
+          title: "Weddings",
+          image: weddings,
+          description: "In the new era of technology we look in the future with certainty for life."
+        },
+        {
+          title: "Events",
+          image: events,
+          description: "In the new era of technology we look in the future with certainty for life."
+        }
+
+      ]
+    }
+  }
+
+}
+</script>
